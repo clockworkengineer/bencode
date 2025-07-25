d3d6a73ef6f6b47df8fa51a917f0ec6d6e9ddc43
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2025-07-25 19:53:21 +0100

    Add more tests.

--- src/bencode_lib/io/sources/source.rs
+++ src/bencode_lib/io/sources/source.rs
@@ -9,15 +9,17 @@ impl Buffer {
     // }
     // pub fn add_byte(&mut self, to_add: u8) {
     // }
+    pub fn to_string(&self) -> String{
+        String::from_utf8_lossy(&self.buffer).into_owned()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::Buffer;
     #[test]
     fn create_source_buffer_works() {
         let  source = Buffer::new(String::from("i32e").as_bytes());
-
+        assert_eq!(source.to_string(), "i32e");
     }
 }
