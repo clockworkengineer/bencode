03165660273c3a7188e7daf4819c33228348384c
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2025-08-08 18:44:02 +0100

    refactor

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,8 +1,12 @@
+pub mod bencode_lib;
+
+pub use bencode_lib::io::sources::buffer::Buffer as BufferSource;
+pub use bencode_lib::io::destinations::buffer::Buffer as BufferDestination;
+pub use bencode_lib::io::sources::file::File as FileSource;
+pub use bencode_lib::io::destinations::file::File as FileDestination;
+
+pub use bencode_lib::nodes::node::Node as Node;
+
 pub use bencode_lib::stringify::default::stringify as stringify;
 pub use bencode_lib::parser::default::parse as parse;
-pub use bencode_lib::io::destinations::file::File as FileDestination;
-pub use bencode_lib::io::sources::file::File as FileSource;
-pub use bencode_lib::io::destinations::buffer::Buffer as BufferDestination;
-pub use bencode_lib::io::sources::buffer::Buffer as BufferSource;
-pub use bencode_lib::nodes::node::Node as Node;
-pub mod bencode_lib;
+
