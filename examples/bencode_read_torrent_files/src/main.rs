2fb3bf18f0ecd74589c5e72fbf64f179fbb39916
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2025-08-12 14:58:22 +0100

    example refactor.

--- examples/bencode_read_torrent_files/src/main.rs
+++ examples/bencode_read_torrent_files/src/main.rs
@@ -26,18 +26,18 @@ struct TorrentFile {
     files: Vec<FileDetails>,
 }
 
-fn get_integer(dict: &std::collections::HashMap<String, Node>, key: &str) -> u64 {
+fn get_integer(dict: &std::collections::HashMap<String, Node>, key: &str, default: u64) -> u64 {
     if let Some(Node::Integer(n)) = dict.get(key) {
         return *n as u64;
     }
-    0
+    default
 }
 
-fn get_string(dict: &std::collections::HashMap<String, Node>, key: &str) -> String {
+fn get_string(dict: &std::collections::HashMap<String, Node>, key: &str, default: &str) -> String {
     if let Some(Node::Str(s)) = dict.get(key) {
         return s.clone();
     }
-    String::new()
+    default.to_string()
 }
 
 fn get_info_integer(dict: &std::collections::HashMap<String, Node>, key: &str) -> u64 {
@@ -81,7 +81,7 @@ fn get_file_list(dict: &HashMap<String, Node>) -> Vec<FileDetails> {
                 .iter()
                 .filter_map(|file| {
                     if let Node::Dictionary(file_dict) = file {
-                        let length = get_integer(file_dict, "length");
+                        let length = get_integer(file_dict, "length", 0);
                         let path = match file_dict.get("path") {
                             Some(Node::List(path_list)) => path_list
                                 .iter()
@@ -125,47 +125,49 @@ fn validate_required_keys(dict: &HashMap<String, Node>) -> Result<(), String> {
     Ok(())
 }
 
+fn print_torrent_details(torrent: &TorrentFile) {
+    println!("Successfully parsed torrent file:");
+    println!("Announce URL: {}", torrent.announce);
+    println!("Announce List URLs:");
+    for url in &torrent.announce_list {
+        println!("  - {}", url);
+    }
+    println!("Encoding: {}", torrent.encoding);
+    println!("Attribute: {}", torrent.attribute);
+    println!("Comment: {}", torrent.comment);
+    println!("Creation Date: {}", torrent.creation_date);
+    println!("Created By: {}", torrent.created_by);
+    println!("Length: {} bytes", torrent.length);
+    println!("Name: {}", torrent.name);
+    println!("Piece Length: {}", torrent.piece_length);
+    println!("Pieces: {}", torrent.pieces.as_bytes().iter().map(|b| format!("{:02x}", b)).collect::<String>());
+    println!("Private Bit Mask: {}", torrent.private_flag);
+    println!("Source: {}", torrent.source);
+    println!("Files:");
+    for file in &torrent.files {
+        println!("  - {} ({} bytes)", file.path, file.length);
+    }
+}
 fn read_torrent_file(path: &Path) -> Result<TorrentFile, String> {
     match FileSource::new(path.to_str().unwrap()) {
         Ok(mut file) => match parse(&mut file) {
             Ok(Node::Dictionary(dict)) => {
                 validate_required_keys(&dict)?;
-
-                let announce = get_string(&dict, "announce");
-                let announce_list = get_announce_list(&dict);
-
-                let encoding = match dict.get("encoding") {
-                    Some(Node::Str(s)) => s.clone(),
-                    _ => "UTF-8".to_string(),
-                };
-
-                let comment = get_string(&dict, "comment");
-                let creation_date = get_integer(&dict, "creation date");
-                let created_by = get_string(&dict, "created by");
-                let attribute = get_info_integer(&dict, "attribute");
-                let length = get_info_integer(&dict, "length");
-                let name = get_info_string(&dict, "name");
-                let piece_length = get_info_integer(&dict, "piece length");
-                let pieces = get_info_string(&dict, "pieces");
-                let private_flag = get_info_integer(&dict, "private");
-                let source = get_info_string(&dict, "source");
-                let files = get_file_list(&dict);
-
                 Ok(TorrentFile {
-                    announce,
-                    announce_list,
-                    encoding,
-                    attribute,
-                    comment,
-                    creation_date,
-                    created_by,
-                    length,
-                    name,
-                    piece_length,
-                    pieces,
-                    private_flag,
-                    source,
-                    files,
+                    announce: get_string(&dict, "announce", ""),
+                    announce_list : get_announce_list(&dict),
+                    encoding : get_string(&dict,"encoding","UTF-8"),
+                    attribute : get_info_integer(&dict, "attribute"),
+                    comment: get_string(&dict, "comment", ""),
+                    creation_date : get_integer(&dict, "creation date", 0),
+                    created_by : get_string(&dict, "created by", ""),
+                    length: get_info_integer(&dict, "length"),
+                    name : get_info_string(&dict, "name"),
+                    piece_length : get_info_integer(&dict, "piece length"),
+                    pieces: get_info_string(&dict, "pieces"),
+                    private_flag :  get_info_integer(&dict, "private"),
+                    source : get_info_string(&dict, "source"),
+                    files : get_file_list(&dict),
                 })
             }
             Err(s)=> Err(s),
@@ -184,27 +186,7 @@ fn main() {
                 println!("\nProcessing {:?}:", path);
                 match read_torrent_file(&path) {
                     Ok(torrent) => {
-                        println!("Successfully parsed torrent file:");
-                        println!("Announce URL: {}", torrent.announce);
-                        println!("Announce List URLs:");
-                        for url in &torrent.announce_list {
-                            println!("  - {}", url);
-                        }
-                        println!("Encoding: {}", torrent.encoding);
-                        println!("Attribute: {}", torrent.attribute);
-                        println!("Comment: {}", torrent.comment);
-                        println!("Creation Date: {}", torrent.creation_date);
-                        println!("Created By: {}", torrent.created_by);
-                        println!("Length: {} bytes", torrent.length);
-                        println!("Name: {}", torrent.name);
-                        println!("Piece Length: {}", torrent.piece_length);
-                        println!("Pieces: {}", torrent.pieces.as_bytes().iter().map(|b| format!("{:02x}", b)).collect::<String>());
-                        println!("Private Bit Mask: {}", torrent.private_flag);
-                        println!("Source: {}", torrent.source);
-                        println!("Files:");
-                        for file in &torrent.files {
-                            println!("  - {} ({} bytes)", file.path, file.length);
-                        }
+                        print_torrent_details(&torrent);
                     }
                     Err(e) => eprintln!("Error reading torrent file: {}", e),
                 }
